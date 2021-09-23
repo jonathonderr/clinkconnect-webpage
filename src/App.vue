f75c7672f6f956a89ad4ad00a2344fae97fb96e7
jonathonderr <jdog.derr1108@gmail.com>
2021-09-23 13:19:58 -0700

    Added CNAME config and privacy policy

--- src/App.vue
+++ src/App.vue
@@ -5,87 +5,43 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Clink Logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo.png"
-          transition="scale-transition"
-          width="40"
-        />
-        <h2>Clink</h2>
-      </div>
+      <router-link to="/">
+        <v-btn
+            href=""
+            target="_blank"
+            text
+            x-large
+        >
+          <div class="d-flex align-center">
+            <v-img
+              alt="Clink Logo"
+              class="shrink mr-2"
+              contain
+              src="./assets/logo.png"
+              transition="scale-transition"
+              width="40"
+            />
+            <h2>Clink</h2>
+          </div>
+        </v-btn>
+      </router-link>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://clinkconnect.app/privacy"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Privacy Policy</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <router-link to="/privacy">
+        <v-btn
+          href=""
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Privacy Policy</span>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </router-link>
     </v-app-bar>
 
     <v-main class="background-color">
-      <v-row class="fill-height" justify="center" no-gutters>
-        <v-col cols="12" md="6">
-          <v-row class="fill-height" align="center" justify="center" no-gutters>
-            <v-col cols="8">
-              <v-col v-if="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm" cols="12" >
-                <v-container></v-container>
-              </v-col>
-              <v-row justify="start">
-                <h1>Clink</h1>
-              </v-row>
-              <v-row justify="start">
-                <v-card
-                  width="70%"
-                  height="10"
-                  color="primary"
-                  elevation="0"
-                >
-                </v-card>
-              </v-row>
-              <v-row>
-                <v-container />
-              </v-row>
-              <v-row justify="center">
-                <body>
-                  Using Clink is best way to break down social barriers, meet new people,
-                  and experience being in the moment. Clink utilizes Bluetooth technology
-                  to allow you to message and connect with the people around with you. 
-                  Download the app and start connecting!
-                </body>
-              </v-row>
-              <v-row>
-                <v-container />
-              </v-row>
-              <v-row>
-                <a href="https://apps.apple.com/app/id1570368363/">
-                  <v-img 
-                    src="./assets/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
-                  />
-                </a>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col v-if="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm" cols="12" >
-          <v-container></v-container>
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-row class="fill-height" align="end" justify="end" no-gutters>
-            <v-img
-              class="mockup"
-              aspect-ratio="1"
-              src="./assets/deviceMockup.png"
-            ></v-img>
-          </v-row>
-        </v-col>
-      </v-row>
+      <router-view></router-view>
     </v-main>
     <v-footer
       color="gray"
@@ -112,13 +68,13 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
+// import Home from './components/home.vue'
 
 export default {
   name: 'Clink Connect',
 
   components: {
-    // HelloWorld,
+    // Home,
   },
 
   data: () => ({
---
+++ src/components/privacy.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Privacy
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Privacy',
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+ .debug-ui {
+   background-color: red;
+ }
+
+ .background-color {
+   background-color: #E9EAE9;
+ }
+
+ .mockup {
+    height: 85vh;
+ }
+</style>
